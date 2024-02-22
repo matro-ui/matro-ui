7d62444081578679c8f51c08c2e9edf4bd48ccec
P Jaworski <pjaworski.dev@gmail.com>
2024-02-22 16:34:56 +0100

    feat: added Stack component

---
+++ dist/layouts/Stack/Stack.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import  s from './index.module.css';
+import classNames from '../../../tools/classNames';
+
+
+const Stack = (props) => {
+  const inlineStyles = {
+    'gap': props.gap,
+    'flex-wrap': props.wrap === "reverse" ? "wrap-reverse" : props.wrap ? "wrap" : "nowrap",
+    ...props.style
+  }
+  const itemProps = {...props};
+  delete itemProps.children;
+
+  if(props.children) return (
+    <div 
+    {...itemProps}
+    className={classNames({
+      ["stack"] : true,
+      [s.stack]: true,
+      [s.row]: props.direction === "row",
+      [itemProps.className]: itemProps.className
+    })}
+    style={inlineStyles}
+    >{props.children}</div>
+  );
+  return ""
+}
+
+export default Stack
