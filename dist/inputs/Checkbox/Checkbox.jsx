3ef890e1ee332090bffd6689291e0b20f3ee2d43
P Jaworski <pjaworski.dev@gmail.com>
2024-02-23 22:26:30 +0100

    build: updated checkbox
    Added disabled, checked props & auto binding if not provided

--- dist/inputs/Checkbox/Checkbox.jsx
+++ dist/inputs/Checkbox/Checkbox.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import * as s from './index.module.css';
+import classNames from '../../../tools/classNames';
 
 const defaultProps = {
   value: false,
@@ -8,43 +9,70 @@ const defaultProps = {
 }
 
 const Checkbox = (props = defaultProps) => {
-  const {name, value} = props;
-  const onChange = (e) => props.onChange(!!e.target.checked);
+  const {name, value, disabled, checked} = props;
+  const defaultValue = value || checked || "";
+  const [init, setInit] = React.useState(false);
+  const [valueLoc, setValueLoc] = React.useState(defaultValue);
+
+  const onChange = (e) => {
+    if(!disabled) {
+      if(props.onChange) 
+        props.onChange(!!e.target.checked);
+      else 
+        setValueLoc(!!e.target.checked);
+    }
+  } 
+
+  // To prevent init anim
+  React.useEffect(() => {
+    setInterval(() => setInit(true), 200);
+  })
+
   return (
-    <label className={s.parent}>
+    <label
+      className={classNames({
+        ["mtui-checkbox"]: true,
+        [s.parent]: true,
+        [s.disabled]: disabled
+      })}
+    >
       {/* input hidden  */}
       <input {...{
         type: "checkbox", 
-        checked: value, 
+        checked: props.onChange ? defaultValue : valueLoc, 
         className: s.input,
         onChange, 
         name}}/>
 
       {/* input visible  */}
       <span className={s.checkbox}>
-      {value ? (
-  <svg 
-    className={s.svg}
-    viewBox="0 0 11 11" 
-    style={{
-      display: "inline-block", 
-      fill: "none", 
-      strokeWidth: 2, 
-      stroke: "currentcolor", 
-    }}
-  >
-    <polyline>
-        <animate
-        attributeName="points"
-        dur="200ms"
-        from="2,4 5,7 5,7"
-        to="2,5 5,8 9.5,3"
-        fill="freeze"
-        begin="0ms"/>
-    </polyline>
-  </svg>
-) : null}
+      {(props.onChange ? defaultValue : valueLoc) ? (
+        <svg 
+          className={s.svg}
+          viewBox="0 0 11 11" 
+          style={{
+            display: "inline-block", 
+            fill: "none", 
+            strokeWidth: 2, 
+            stroke: "currentcolor", 
+          }}
+        >
 
+        {init ? 
+        <polyline>
+              <animate
+              attributeName="points"
+              dur="200ms"
+              from="2,4 5,7 5,7"
+              to="2,5 5,8 9.5,3"
+              fill="freeze"
+              begin="0ms"/>
+          </polyline>
+          : 
+          <polyline points="2,5 5,8 9.5,3"/>}
+
+        </svg>
+      ) : null}
       </span>
 
       {/* label  */}
