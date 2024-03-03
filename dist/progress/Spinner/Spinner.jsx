8a9bee777a767199afef16c64abc296287926035
P Jaworski <pjaworski.dev@gmail.com>
2024-03-03 16:38:05 +0100

    update: added clsx & color, hue, size prop options

--- dist/progress/Spinner/Spinner.jsx
+++ dist/progress/Spinner/Spinner.jsx
@@ -1,16 +1,39 @@
 import React from 'react'
 import s from './index.module.css';
-import classNames from '../../../tools/classNames';
+import clsx from 'clsx';
+import { colorToHue } from '../../../context';
 
 const Spinner = (props) => {
-  return (
+  const { size, colorScheme, hidden, color } = props;
+
+  const inlineStyles = {
+    '--hue': (colorToHue[colorScheme] > -1) ? colorToHue[colorScheme]
+      : props.hue ? props.hue
+        : 220,
+    ...props.style
+  }
+
+  if (color?.indexOf("#") > -1 || color === "inherit")
+    inlineStyles["--color"] = color;
+
+  if (hidden) return "";
+
+  else return (
     <div
-      className={classNames({
-        "mtui-spinner": true,
-        [s.parent]: true,
-        [props.className]: !!props.className,
-      })}
-      style={props.styles}
+      className={clsx([
+        "mtui-spinner",
+        s.parent,
+        {
+          [s.size_xs]: size === "xs" || size === "XS",
+          [s.size_s]: size === "s" || size === "S",
+          [s.size_m]: size === "m" || size === "M",
+          [s.size_l]: size === "l" || size === "L",
+          [s.size_xl]: size === "xl" || size === "XL",
+          [s.isColor]: !!props.colorScheme,
+          [props.className]: !!props.className,
+        }
+      ])}
+      style={{ ...inlineStyles, ...props.styles }}
     >
       <span className={s.act} />
       <span className={s.text}>Loading...</span>
